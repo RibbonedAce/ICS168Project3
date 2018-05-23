2a0117c901c269787132fc27b37f9e0b54bcc33f
Unknown <tylergemora@yahoo.com>
2018-05-23 16:56:30 -0700

    Network manager

---
+++ Assets/Scripts/GameNetworkManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameNetworkManager : NetworkManager 
+{
+	#region Variables
+	
+	#endregion
+	
+	#region Properties
+	
+	#endregion
+	
+	#region Events
+    /// <summary>
+    /// Awake is called before Start
+    /// </summary>
+	private void Awake() 
+	{
+		
+	}
+
+    /// <summary>
+    ///  Use this for initialization
+    /// </summary>
+    private void Start() 
+	{
+		
+	}
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    private void Update() 
+	{
+		
+	}
+	#endregion
+	
+	#region Methods
+	
+	#endregion
+	
+	#region Coroutines
+	
+	#endregion
+}
