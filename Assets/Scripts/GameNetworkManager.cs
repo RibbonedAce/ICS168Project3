02a5acf9d99a5b3550b43b04893c367a276d0830
Unknown <tylergemora@yahoo.com>
2018-05-25 10:54:09 -0700

    Flexible Network HUD

--- Assets/Scripts/GameNetworkManager.cs
+++ Assets/Scripts/GameNetworkManager.cs
@@ -40,10 +40,75 @@ public class GameNetworkManager : NetworkManager
 	#endregion
 	
 	#region Methods
-	
-	#endregion
-	
-	#region Coroutines
-	
-	#endregion
+    /// <summary>
+    /// Void return function of StartHost()
+    /// </summary>
+	public void VoidStartHost()
+    {
+        StartHost();
+    }
+
+    /// <summary>
+    /// Void return function of StartClient()
+    /// </summary>
+	public void VoidStartClient()
+    {
+        StartClient();
+    }
+
+    /// <summary>
+    /// Void return function of StartServer()
+    /// </summary>
+	public void VoidStartServer()
+    {
+        StartServer();
+    }
+
+    /// <summary>
+    /// Set the manager's match name
+    /// </summary>
+    /// <param name="value">The name of the match</param>
+    public void ChangeMatchName(string value)
+    {
+        matchName = value;
+    }
+
+    /// <summary>
+    /// Ready the client and add the player
+    /// </summary>
+    public void Ready()
+    {
+        ClientScene.Ready(client.connection);
+        if (ClientScene.localPlayers.Count == 0)
+            ClientScene.AddPlayer((short)0);
+    }
+
+    /// <summary>
+    /// Create a simple matchmaking match
+    /// </summary>
+    public void CreateMatch()
+    {
+        matchMaker.CreateMatch(matchName, matchSize, true, "", "", "", 0, 0, OnMatchCreate);
+    }
+
+    /// <summary>
+    /// List all of the matches available
+    /// </summary>
+    public void ListMatches()
+    {
+        matchMaker.ListMatches(0, 20, "", false, 0, 0, OnMatchList);
+    }
+
+    /// <summary>
+    /// Set the matches to null
+    /// </summary>
+    public void SetMatchesNull()
+    {
+        matches = null;
+    }
+    #endregion
+
+    #region Coroutines
+
+    #endregion
 }
---
+++ Assets/Scripts/NetHUD.cs
@@ -0,0 +1,220 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(NetworkManager))]
+public class NetHUD : MonoBehaviour
+{
+    #region Variables
+    /// <summary>
+    /// <para>The set of elements for the HUD</para>
+    /// </summary>
+    [SerializeField]
+    private GameObject[] elements;
+
+    /// <summary>
+    /// <para>The set of text labels for the HUD</para>
+    /// </summary>
+    [SerializeField]
+    private Text[] labels;
+
+    /// <summary>
+    /// <para>The button prefab to spawn for matches</para>
+    /// </summary>
+    [SerializeField]
+    private GameObject buttonPrefab;
+
+    /// <summary>
+    /// <para>The network manager to reference</para>
+    /// </summary>
+    private NetworkManager manager;
+
+    /// <summary>
+    /// <para>Whether the server is being shown</para>
+    /// </summary>
+    private bool m_ShowServer;
+
+    /// <summary>
+    /// The match buttons instantiated
+    /// </summary>
+    private List<GameObject> matchButtons;
+
+    /// <summary>
+    /// The match info snapshot to keep track of.
+    /// </summary>
+    private UnityEngine.Networking.Match.MatchInfoSnapshot currentMatch;
+	#endregion
+	
+	#region Properties
+
+	#endregion
+	
+	#region Events
+    /// <summary>
+    /// Awake is called before Start
+    /// </summary>
+	private void Awake() 
+	{
+        manager = GetComponent<NetworkManager>();
+        matchButtons = new List<GameObject>();
+	}
+
+    /// <summary>
+    ///  Use this for initialization
+    /// </summary>
+    private void Start()
+    {
+
+	}
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    private void Update() 
+	{
+        List<int> actives = new List<int>();
+
+        foreach (GameObject mb in matchButtons)
+        {
+            Destroy(mb);
+        }
+
+        bool flag = manager.client == null || manager.client.connection == null || manager.client.connection.connectionId == -1;
+        if (!manager.IsClientConnected() && !NetworkServer.active && manager.matchMaker == null)
+        {
+            if (flag)
+            {
+                actives.Add(0);
+            }
+            else
+            {
+                actives.Add(1);
+                labels[0].text = "Connecting to " + manager.networkAddress + ":" + manager.networkPort + "..";
+            }
+        }
+        else
+        {
+            if (NetworkServer.active)
+            {
+                string text = "Server: port=" + manager.networkPort;
+                if (manager.useWebSockets)
+                    text += " (Using WebSockets)";
+                actives.Add(2);
+                labels[1].text = text;
+            }
+            if (manager.IsClientConnected())
+            {
+                actives.Add(3);
+                labels[2].text = "Client: address=" + manager.networkAddress + " port=" + manager.networkPort;
+            }
+        }
+        if (manager.IsClientConnected() && !ClientScene.ready)
+        {
+            actives.Add(4);
+        }
+        if (NetworkServer.active || manager.IsClientConnected())
+        {
+            actives.Add(5);
+        }
+        if (!(NetworkServer.active || manager.IsClientConnected() || !flag))
+        {
+            if (manager.matchMaker == null)
+            {
+                actives.Add(6);
+            }
+            else
+            {
+                if (manager.matchInfo == null)
+                {
+                    if (manager.matches == null)
+                    {
+                        actives.Add(7);
+                        labels[3].text = "Room Name:";
+                    }
+                    else
+                    {
+                        actives.Add(8);
+                        foreach (UnityEngine.Networking.Match.MatchInfoSnapshot m in manager.matches)
+                        {
+                            currentMatch = m;
+                            MakeMatchButton();
+                        }
+                    }
+                }
+                actives.Add(9);
+                if (m_ShowServer)
+                {
+                    actives.Add(10);
+                }
+                actives.Add(11);
+                labels[4].text = "MM Uri: " + manager.matchMaker.baseUri;
+            }
+        }
+
+        for (int i = 0; i < elements.Length; ++i)
+        {
+            elements[i].SetActive(actives.Contains(i));
+        }
+    }
+
+
+	#endregion
+	
+	#region Methods
+    /// <summary>
+    /// Toggle whether the server's shown
+    /// </summary>
+    public void ToggleServer()
+    {
+        m_ShowServer = !m_ShowServer;
+    }
+
+    /// <summary>
+    /// Instantiate a button to join the currently tracked match
+    /// </summary>
+    public void MakeMatchButton()
+    {
+        GameObject g = Instantiate(buttonPrefab, elements[8].GetComponentInChildren<VerticalLayoutGroup>().transform);
+        matchButtons.Add(g);
+        g.GetComponentInChildren<Text>().text = "Join Match:" + currentMatch.name;
+        g.GetComponent<Button>().onClick.AddListener(SetMatchInfo);
+    }
+
+    /// <summary>
+    /// Set the manager's info for the currently tracked match
+    /// </summary>
+    private void SetMatchInfo()
+    {
+        manager.matchName = currentMatch.name;
+        manager.matchSize = (uint)currentMatch.currentSize;
+        manager.matchMaker.JoinMatch(currentMatch.networkId, "", "", "", 0, 0, manager.OnMatchJoined);
+    }
+
+    /// <summary>
+    /// Set the match host to a preset choice
+    /// </summary>
+    /// <param name="choice">The type of match to set the manager to</param>
+    public void SetMatchType(int choice)
+    {
+        m_ShowServer = false;
+        switch (choice)
+        {
+            case 0:
+                manager.SetMatchHost("localhost", 1337, false);
+                return;
+            case 1:
+                manager.SetMatchHost("mm.unet.unity3d.com", 443, true);
+                return;
+            case 2:
+                manager.SetMatchHost("staging-mm.unet.unity3d.com", 443, true);
+                return;
+        }
+    }
+    #endregion
+
+    #region Coroutines
+
+    #endregion
+}
