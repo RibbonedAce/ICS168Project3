6945b3a43996cb98b72eefdebb7abad3d5120d88
khoan8 <khoan8@uci.edu>
2018-05-28 12:28:34 -0700

    added field, building types, and player Ancient Bulding

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class PlayerController : NetworkBehaviour {
+
+#region Attributes
+    private float Health;
+
+    public List<GameObject> Buildings;
+#endregion
+
+    void Awake()
+    {
+        transform.position = new Vector3(-8.5f,1f,0);
+        Buildings = new List<GameObject>();
+        Health = 50f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!isLocalPlayer)
+            return;
+        Build();
+	}
+
+    private void Build()
+    {
+    }
+}
